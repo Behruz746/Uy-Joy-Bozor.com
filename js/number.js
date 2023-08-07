720dc2bcedd40357a8bf50c002c2148fa0130361
Behruz <>
2023-08-07 21:00:45 +0500

    new element

--- js/number.js
+++ js/number.js
@@ -2,15 +2,12 @@ const toggleElement = {'once': true};
 const numbersBtn = document.getElementById('numbersBtn');
 
 function creatNewNum() {
-    const parentEl = numbersBtn.parentElement.parentElement;
+    const parentEl = numbersBtn.parentElement.parentElement; 
 
     parentEl.innerHTML += `
+        <h3 class="input__title" style="margin-top: 10px;">Qoʻshimcha telefon raqami</h3>
         <div class="number__input" style="margin-top: 10px;">
             <input type="text" placeholder="Masalan, “99 123 45 67”." class="form__input" id="number-input">
-
-            <!--<div id="numbersBtn">
-                <img src="./assets/images/svg/pluse02.svg" alt="pluse icon">
-            </div>-->
         </div>
     `;
 };
