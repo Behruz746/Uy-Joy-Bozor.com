1f8f764a04185cb972156f9920acb2337f783a92
Behruz <>
2023-08-08 22:46:32 +0500

    new function for btn submit

--- js/main.js
+++ js/main.js
@@ -3,24 +3,40 @@ import './imgFile.js';
 import './number.js';
 
 const form = document.getElementById('form-data');
+const selectInput = document.getElementById('selectInput');
+
+const regEx = /^[a-zA-Z0-9]{6,100}$/;
+
+let [
+    descriptionInput, 
+    roomInput, 
+    sizeInput, 
+    repairInput, 
+    additionInput, 
+    fromInput, 
+    priceInput, 
+    numberInput ] = [
+    form['description-input'], 
+    form['room-input'].value,
+    form['size-input'].value,
+    form['repair-input'].value,
+    form['addition-input'].value,
+    form['from-input'].value,
+    form['price-input'].value,
+    form['number-input'].value,
+];
 
 function formSubmit(e) {
     e.preventDefault();
 
-    let [descriptionInput, roomInput, sizeInput, repairInput, additionInput, fromInput, priceInput, numberInput] = [
-        form['description-input'].value, 
-        form['room-input'].value,
-        form['size-input'].value,
-        form['repair-input'].value,
-        form['addition-input'].value,
-        form['from-input'].value,
-        form['price-input'].value,
-        form['number-input'].value,
-    ];
+    let parnetEl = descriptionInput.parentElement.children[2];
+    let result = regEx.test(descriptionInput.value);
 
-    console.log(descriptionInput);
-    console.log(roomInput);
-
+    if(result) {
+        parnetEl.style.display = 'none';
+    } else {
+        parnetEl.style.display = 'block';
+    }
 };
 
 form.addEventListener('submit', formSubmit);
