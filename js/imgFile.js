a1279740f2675c7fd94e675d6a8757ec0be727f9
Behruz <>
2023-08-11 19:04:12 +0500

    new url file functions added

--- js/imgFile.js
+++ js/imgFile.js
@@ -1,4 +1,5 @@
 const imageCard = document.querySelectorAll('.image__card');
+const imageRow = document.querySelector('.form-image__row');
 const creatCard = document.querySelector('.creat--card');
 const imageFileInput = document.getElementById('file-01');
 
@@ -6,17 +7,39 @@ function addImageFile() {
   creatCard.style.order = '1';
   let imageUrl = URL.createObjectURL(imageFileInput.files[0]);
 
+  const imageCardEl = document.createElement('div');
+  imageCardEl.classList.add('image__card');
+  imageRow.append(imageCardEl);
+
+  const inputEl = document.createElement('input');
+  inputEl.type = 'file';
+  inputEl.name = 'upDateFile';
+  inputEl.classList.add('inputNewFile');
+  imageCardEl.append(inputEl);
+
   const imageEl = document.createElement('img');
   imageEl.src = imageUrl;
   imageEl.alt = "home image";
   imageEl.classList.add('imageEl');
-  // imageCard.appendChild(imageEl);
+  imageCardEl.appendChild(imageEl);
+  const updateInput = document.querySelectorAll('.inputNewFile');
 
-  imageCard.forEach(item=> {
-    item.appendChild(imageEl);
+  updateInput.forEach((item)=> item.addEventListener('change', ()=> {
+    let newUrl = URL.createObjectURL(item.files[0]);
+    imageEl.src = newUrl;
+  }));
+
+  updateInput.forEach((item, index)=> {
+    if(index >= 4) {
+      console.log('5 card maked');
+      creatCard.remove();
+      document.querySelector('.card__err').classList.remove('remove--add');
+    }
+
+    if(index === 0) {
+      imageCard.forEach(item=> item.remove());
+    }
   });
-
-  console.log(imageUrl, imageEl);
 };
 
 imageFileInput.addEventListener('change', addImageFile);
