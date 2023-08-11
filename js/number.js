ff23c87a0018d61beb46bdbce100110c3ee2ec7a
Behruz <>
2023-08-11 23:32:03 +0500

    new function

--- js/number.js
+++ js/number.js
@@ -1,19 +1,15 @@
-const formNumberFirst = document.querySelector('.form-number__first img');
-const formNumberLast = document.querySelector('.form-number__last');
+const formNumberFirst = document.querySelector(".form-number__first img");
+const formNumberLast = document.querySelector(".form-number__last");
 
 let toggle = false;
-
 function toggleNum() {
-
-    if(!toggle) {
-        formNumberLast.classList.remove('remove--add');
-        formNumberFirst.style.transform = 'rotate(130deg)';
-        toggle = true;
-    } else if(toggle) {
-        formNumberLast.classList.add('remove--add');
-        formNumberFirst.style.transform = 'rotate(0deg)';
-        toggle = false;
-    }
+  if (!toggle) {
+    formNumberLast.classList.remove("remove--add");
+    toggle = true;
+  } else if (toggle) {
+    formNumberLast.classList.add("remove--add");
+    toggle = false;
+  }
 }
 
-formNumberFirst.addEventListener('click', toggleNum);
+formNumberFirst.addEventListener("click", toggleNum);
