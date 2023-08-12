2a52c26c1d4c1e853aa36078ffba75518276b066
Behruz <>
2023-08-12 10:10:55 +0500

    new input type

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
 import './btn.js';
 import './imgFile.js';
 import './number.js';
+
+const mainForm = document.forms.mainForm;
+const elements = mainForm.elements;
+
+function submitForm(event) {
+    event.preventDefault();
+   
+    
+
+};
+
+mainForm.addEventListener('submit', submitForm);
+
+
+
+const focusEl = document.querySelectorAll('.focus');
+
+const userAd = elements.userAd;
+const userAdPlaceholder = elements.userAd.placeholder;
+
+focusEl.forEach((item)=> {
+    let itemPl = item.placeholder;
+
+    item.addEventListener('focus', ()=> {
+        item.placeholder = '';
+    })
+
+    item.addEventListener('blur', ()=> {
+        item.placeholder = itemPl;
+    })
+});
