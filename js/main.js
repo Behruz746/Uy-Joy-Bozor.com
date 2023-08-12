c0038743c79ab3c86c227c5f49173f7cf2018009
Behruz <>
2023-08-12 17:46:04 +0500

    new submit regex

--- js/main.js
+++ js/main.js
@@ -5,26 +5,41 @@ import './number.js';
 const mainForm = document.forms.mainForm;
 const elements = mainForm.elements;
 const focusEl = document.querySelectorAll('.focus');
+const elementsUserAd = elements.userAd;
+const elementsUserRoom = elements.userRoom;
+const elementsUserEstate = elements.userEstate;
+const elementsUserFix = elements.userFix;
+const elementsUserNumberFirst = elements.userNumberFirst;
+const elementsUserNumberLast = elements.userNumberLast;
 
 function submitForm(event) {
-    event.preventDefault();
+    // regex date //
+    let regex = /^[a-zA-Z0-9а-яА-Я\s]{10,}$/;
+    let resultUserAd = regex.test(elementsUserAd.value);
+    let resultUserFix = regex.test(elementsUserFix.value);
+    
+    if(resultUserAd) {
+        console.log(true);
+        document.querySelector('.userAd__err').classList.add('remove--add');
+    } else if(!resultUserAd) {
+        event.preventDefault();
+        document.querySelector('.userAd__err').classList.remove('remove--add');
+        elementsUserAd.value = '';
+    };
 
-    const elementsUserAd = elements.userAd;
-    const elementsUserRoom = elements.userRoom;
-    const elementsUserEstate = elements.userEstate;
-    const elementsUserFix = elements.userFix;
-    const elementsUserNumberFirst = elements.userNumberFirst;
-    const elementsUserNumberLast = elements.userNumberLast;
-
-    // console.log(elementsUserAd, elementsUserRoom, elementsUserEstate, elementsUserFix);
-    console.log(elementsUserNumberFirst.value, '\n', elementsUserNumberLast.value);
+    if(resultUserFix) {
+        document.querySelector('.userFixed__err').classList.add('remove--add');
+    } else if(!resultUserFix) {
+        event.preventDefault();
+        document.querySelector('.userFixed__err').classList.remove('remove--add');
+        elementsUserFix.value = '';
+    };  
 };
 
 mainForm.addEventListener('submit', submitForm);
 
 focusEl.forEach((item, index)=> {
     let itemPl = item.placeholder;
-    let itemVa = item.value;
 
     item.addEventListener('focus', ()=> {
         item.placeholder = '';
