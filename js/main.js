109c51bddb0571b09c618464e3e8b439cc563277
Behruz <>
2023-08-12 11:14:54 +0500

    new tel input functions

--- js/main.js
+++ js/main.js
@@ -4,28 +4,34 @@ import './number.js';
 
 const mainForm = document.forms.mainForm;
 const elements = mainForm.elements;
+const focusEl = document.querySelectorAll('.focus');
 
 function submitForm(event) {
     event.preventDefault();
-   
-    
 
+    const elementsUserAd = elements.userAd;
+    const elementsUserRoom = elements.userRoom;
+    const elementsUserEstate = elements.userEstate;
+    const elementsUserFix = elements.userFix;
+    const elementsUserNumberFirst = elements.userNumberFirst;
+    const elementsUserNumberLast = elements.userNumberLast;
+
+    // console.log(elementsUserAd, elementsUserRoom, elementsUserEstate, elementsUserFix);
+    console.log(elementsUserNumberFirst.value, '\n', elementsUserNumberLast.value);
 };
 
 mainForm.addEventListener('submit', submitForm);
 
-
-
-const focusEl = document.querySelectorAll('.focus');
-
-const userAd = elements.userAd;
-const userAdPlaceholder = elements.userAd.placeholder;
-
-focusEl.forEach((item)=> {
+focusEl.forEach((item, index)=> {
     let itemPl = item.placeholder;
+    let itemVa = item.value;
 
     item.addEventListener('focus', ()=> {
         item.placeholder = '';
+       
+        if(index === 7 || index === 8) {
+            item.value = '+998';
+        }
     })
 
     item.addEventListener('blur', ()=> {
