12c118808300573773ca3c3b31a616a7d532a793
Behruz <>
2023-08-14 18:24:22 +0500

    new function

--- js/btn.js
+++ js/btn.js
@@ -1,19 +1,59 @@
-const btnFirst = document.querySelector('.data__btn01');
-const btnLast = document.querySelector('.data__btn02');
+const formRoomRow = document.querySelector(".form-room__row");
+const btnFirst = document.querySelector(".data__btn01");
+const btnLast = document.querySelector(".data__btn02");
 
-const overBtn = ()=> {
-    btnFirst.classList.add('over');
-    btnFirst.classList.remove('out');
-    btnLast.classList.remove('over');
-    btnLast.classList.add('out');
+const removeEls = document.querySelectorAll('.remove--active');
+
+function homeDateSubmit() {
+  console.log(true);
+    removeEls.forEach((item, index)=> {
+        if(index == 0) {
+            item.innerHTML = `
+                <label for="userRoom">Xonalar soni nechta</label>
+                <input tabindex="4" type="number" class="focus" min="1" max="100" name="userRoom" placeholder="Masalan, “5ta”.">
+                <p class="userRoom__err error__text remove--add">Qo’shimcha ma’lumot kiriting!</p>
+            `;
+        } else if(index == 1) {
+            item.innerHTML = `
+                <label for="userEstate">Tomorqa o’lchami, nechi sotix?</label>
+                <input tabindex="4" type="number" class="focus" min="1" max="100" name="userEstate" placeholder="Masalan, “8 sotix”.">
+                <p class="userEstate__err error__text remove--add">Qo’shimcha ma’lumot kiriting!</p>
+            `;
+        } else if(index == 2) {
+            item.innerHTML = `
+                <label for="userFix">Remont. Xonadan qachon tamirlangan?</label>
+                <input tabindex="5" type="text" class="focus" name="userFix" placeholder="Masalan, “Yaqinda ta’mirdan chiqqan. Biroz kamchiliklari bor”.">
+                <p class="userFixed__err error__text remove--add">Remonti haqida ma’lumot kiriting!</p>
+            `;
+        }
+        item.style.display = 'block';
+    });
+}
+
+function carDateSubmit() {
+  console.log(false);
+
+  removeEls.forEach(item=> {
+    item.innerHTML = "";
+    item.style.display = 'none';
+  });
+}
+
+const overBtn = () => {
+  btnFirst.classList.add("over");
+  btnFirst.classList.remove("out");
+  btnLast.classList.remove("over");
+  btnLast.classList.add("out");
 };
 
-const overBtnLast = ()=> {
-    btnFirst.classList.remove('over');
-    btnFirst.classList.add('out');
-    btnLast.classList.add('over');
-    btnLast.classList.remove('out');
+const overBtnLast = () => {
+  btnFirst.classList.remove("over");
+  btnFirst.classList.add("out");
+  btnLast.classList.add("over");
+  btnLast.classList.remove("out");
 };
 
-btnFirst.addEventListener('mouseover', overBtn);
-btnLast.addEventListener('mouseover', overBtnLast);
+btnFirst.addEventListener("mouseover", overBtn);
+btnLast.addEventListener("mouseover", overBtnLast);
+btnFirst.addEventListener("click", homeDateSubmit);
+btnLast.addEventListener("click", carDateSubmit);
--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-import './btn.js';
-import './imgFile.js';
-import './number.js';
-import './numberDown.js';
+import "./btn.js";
+import "./imgFile.js";
+import "./number.js";
+import "./numberDown.js";
+
+
 
 const mainForm = document.forms.mainForm;
 const elements = mainForm.elements;
 const elementsUserAd = elements.userAd;
+const elementsSelectHome = elements.selectHome;
 const elementsNameFile = elements.nameFile;
 const elementsUserRoom = elements.userRoom;
 const elementsUserEstate = elements.userEstate;
@@ -15,101 +18,107 @@ const ElementUserFrom = elements.userFrom;
 const ElementUserPrice = elements.userPrice;
 const elementsUserNumberFirst = elements.userNumberFirst;
 const elementsUserNumberLast = elements.userNumberLast;
-const focusEl = document.querySelectorAll('.focus');
 
+const focusEl = document.querySelectorAll(".focus");
 
 function submitForm(event) {
-    // regex date //
-    let regex = /^[a-zA-Z0-9а-яА-Я\s]{10,}$/;
-    let resultUserAd = regex.test(elementsUserAd.value);
-    let resultUserFix = regex.test(elementsUserFix.value);
-    let resultUserDate = regex.test(ElementUserDate.value);
-    let resultUserFrom = regex.test(ElementUserFrom.value);
-    
-    if(resultUserAd) {
-        document.querySelector('.userAd__err').classList.add('remove--add');
-    } else if(!resultUserAd) {
-        event.preventDefault();
-        document.querySelector('.userAd__err').classList.remove('remove--add');
-    };
+  // regex date //
+  let regex = /^[a-zA-Z0-9а-яА-Я\s]{10,}$/;
+  let resultUserAd = regex.test(elementsUserAd.value);
+  let resultUserFix = regex.test(elementsUserFix.value);
+  let resultUserDate = regex.test(ElementUserDate.value);
+  let resultUserFrom = regex.test(ElementUserFrom.value);
 
-    if(resultUserFix) {
-        document.querySelector('.userFixed__err').classList.add('remove--add');
-    } else if(!resultUserFix) {
-        event.preventDefault();
-        document.querySelector('.userFixed__err').classList.remove('remove--add');
-    };
+  if (resultUserAd) {
+    document.querySelector(".userAd__err").classList.add("remove--add");
+  } else if (!resultUserAd) {
+    event.preventDefault();
+    document.querySelector(".userAd__err").classList.remove("remove--add");
+  }
 
-    if(elementsUserRoom.value.length === 0) {
-        event.preventDefault();
-        document.querySelector('.userRoom__err').classList.remove('remove--add');
+  if(document.querySelector(".userFixed__err") != null) {
+    if (resultUserFix) {
+      document.querySelector(".userFixed__err").classList.add("remove--add");
+    } else if (!resultUserFix) {
+      event.preventDefault();
+      document.querySelector(".userFixed__err").classList.remove("remove--add");
+    }
+  }
+
+  if(document.querySelector(".userRoom__err") != null) {
+    if (elementsUserRoom.value.length === 0) {
+      event.preventDefault();
+      document.querySelector(".userRoom__err").classList.remove("remove--add");
     } else {
-        document.querySelector('.userRoom__err').classList.add('remove--add');
-    };
+      document.querySelector(".userRoom__err").classList.add("remove--add");
+    }
+  }
 
-    if(elementsUserEstate.value.length === 0) {
-        event.preventDefault();
-        document.querySelector('.userEstate__err').classList.remove('remove--add');
+  if(document.querySelector(".userEstate__err") != null) {
+    if (elementsUserEstate.value.length === 0) {
+      event.preventDefault();
+      document.querySelector(".userEstate__err").classList.remove("remove--add");
     } else {
-        document.querySelector('.userEstate__err').classList.add('remove--add');
-    };
+      document.querySelector(".userEstate__err").classList.add("remove--add");
+    }
+  }
 
-    if(resultUserDate) {
-        document.querySelector('.userDate__err').classList.add('remove--add');
-    } else if(!resultUserDate) {
-        event.preventDefault();
-        document.querySelector('.userDate__err').classList.remove('remove--add');
-    };
+  if (resultUserDate) {
+    document.querySelector(".userDate__err").classList.add("remove--add");
+  } else if (!resultUserDate) {
+    event.preventDefault();
+    document.querySelector(".userDate__err").classList.remove("remove--add");
+  }
 
-    if(resultUserFrom) {
-        document.querySelector('.userFrom__err').classList.add('remove--add');
-    } else if(!resultUserFrom) {
-        event.preventDefault();
-        document.querySelector('.userFrom__err').classList.remove('remove--add');
-    };
+  if (resultUserFrom) {
+    document.querySelector(".userFrom__err").classList.add("remove--add");
+  } else if (!resultUserFrom) {
+    event.preventDefault();
+    document.querySelector(".userFrom__err").classList.remove("remove--add");
+  }
 
-    if(ElementUserPrice.value.length === 0) {
-        event.preventDefault();
-        document.querySelector('.userPrice__err').classList.remove('remove--add');
-    } else {
-        document.querySelector('.userPrice__err').classList.add('remove--add');
-    };
-    
-    if(elementsUserNumberFirst.value.length <= 13) {
-        event.preventDefault();
-        document.querySelector('.userNumFirst__err').classList.remove('remove--add');
-    } else {
-        document.querySelector('.userNumFirst__err').classList.add('remove--add');
-    };
-};
+  if (ElementUserPrice.value.length === 0) {
+    event.preventDefault();
+    document.querySelector(".userPrice__err").classList.remove("remove--add");
+  } else {
+    document.querySelector(".userPrice__err").classList.add("remove--add");
+  }
+
+  if (elementsUserNumberFirst.value.length <= 13) {
+    event.preventDefault();
+    document
+      .querySelector(".userNumFirst__err")
+      .classList.remove("remove--add");
+  } else {
+    document.querySelector(".userNumFirst__err").classList.add("remove--add");
+  }
+}
 
 const maskOptions = {
-    mask: '+998 00 000 0000',
-    lazy: true,
+  mask: "+998 00 000 0000",
+  lazy: true,
 };
-  
+
 const mask = IMask(elementsUserNumberFirst, maskOptions);
 
+focusEl.forEach((item, index) => {
+  let itemPl = item.placeholder;
 
-
-
-focusEl.forEach((item, index)=> {
-    let itemPl = item.placeholder;
-
-    if(index === 7) {
-        item.addEventListener('focus', ()=> {
-            mask.value = '+998' + elementsUserNumberFirst.value.slice(5,);
-        });
-    }
-  
-    item.addEventListener('focus', ()=> {
-      item.placeholder = ''; 
+  if (index === 7) {
+    item.addEventListener("focus", () => {
+      mask.value = "+998" + elementsUserNumberFirst.value.slice(5);
     });
-  
-    item.addEventListener('blur', ()=> {
-      item.placeholder = itemPl;
-    });
+  }
+
+  item.addEventListener("focus", () => {
+    item.placeholder = "";
+  });
+
+  item.addEventListener("blur", () => {
+    item.placeholder = itemPl;
+  });
 });
 
+mainForm.addEventListener("submit", submitForm);
 
-mainForm.addEventListener('submit', submitForm);
+
